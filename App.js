c6174bb049e0ff7bc393f3c75e80aac8162cf740
Essoham SIBABI <essoham.sibabi@helloquid.com>
2021-11-30 20:02:12 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from  "react-redux";
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from "./store";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+              <Stack.Screen
+                name='HomeScreen'
+                component={HomeScreen} 
+                options={{
+                  headerShown: false
+                }} 
+              />
+              <Stack.Screen
+                name='MapScreen'
+                component={MapScreen} 
+                options={{
+                  headerShown: false
+                }} 
+              />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+     
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E5E8E8',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image 
+                 style={{
+                    width: 100,
+                    height: 100,
+                    resizeMode: 'contain',
+                 }}
+                source={{ 
+                    uri: "https://links.papareact.com/gzs",
+                    }}
+                 />
+                <GooglePlacesAutocomplete
+                   placeholder="Where From?"
+                   styles={{
+                       container: {
+                           flex: 0,
+                       },
+                       textInput: {
+                           fontSize: 18,
+                       }
+                   }}
+                   onPress= {(data, details = null) => {
+                    console.log(data);
+                    console.log(details);
+                   }}
+                   fetchDetails={true}
+                   enablePoweredByContainer={false}
+                   minLength={2}
+                   query={{
+                       key: GOOGLE_MAPS_APIKEY,
+                       language: "en",
+                   }}
+                   nearbyPlacesAPI="GooglePlacesSearch" 
+                   debounce={400}
+                />
+
+                 <NavOptions />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
